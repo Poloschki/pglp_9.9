1e42152aedbaafe6b3ae8d7cd6fbf3fec65619d5
Poloschki <patrick77290@gmail.com>
2020-05-04 20:09:52 +0200

    Class Point

---
+++ pglp_9.9/src/main/java/org/example/Point.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Point {
+  double x;
+  double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void transpose(double x, double y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  @Override
+  public String toString() {
+    return "(" +
+        "x=" + x +
+        ", y=" + y +
+        ')';
+  }
+}
