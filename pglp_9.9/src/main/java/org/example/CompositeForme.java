11c3a8f0c06523c4ec506a50f8962372c8e6f89b
Poloschki <patrick77290@gmail.com>
2020-05-04 20:45:26 +0200

    Class CompositeForme qui implémente composite

---
+++ pglp_9.9/src/main/java/org/example/CompositeForme.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class CompositeForme implements Composite {
+  String nom;
+
+  @Override
+  public void print() {
+
+  }
+
+  @Override
+  public void move(double x, double y) {
+
+  }
+}
